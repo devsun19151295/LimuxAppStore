3c46aa20a77f5c922d8749e121afe1f10a84f54f
Thappatan C. (Sun) <thappatan@heartworks.io>
2020-07-27 11:19:25 +0700

    [ init ] first initial project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-drawer
+      show-if-above
+      v-model="left"
+      side="left"
+      behavior="desktop"
+      bordered
+    >
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  //components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    };
+  }
+};
+</script>
