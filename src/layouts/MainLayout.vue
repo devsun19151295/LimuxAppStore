d6b2a8cdca2a6959386d780dfd2549349ebe9f14
devsun19151295 <thappatan@heartworks.io>
2020-07-27 14:57:25 +0700

    [ update ] use menu from menu item component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,12 +2,29 @@
   <q-layout view="hHh lpR fFf">
     <q-drawer
       show-if-above
-      v-model="left"
+      v-model="leftDrawerOpen"
       side="left"
       behavior="desktop"
       bordered
+      :width="230"
     >
       <!-- drawer content -->
+      <div class="row search-box justify-center">
+        <q-input outlined v-model="search" placeholder="search" dense>
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center full-width">
+        <MenuItem
+          v-for="menu in menuItems"
+          :key="menu.title"
+          :title="menu.title"
+          :icon="menu.icon"
+          :to="menu.to"
+        ></MenuItem>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -17,14 +34,38 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import MenuItem from "components/MenuItem";
+
 export default {
   name: "MainLayout",
-  //components: { EssentialLink },
+  components: {
+    MenuItem
+  },
   data() {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      search: "",
+      leftDrawerOpen: false
     };
+  },
+  computed: {
+    ...mapGetters("menu", ["menuItems"])
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.search-box {
+  height: 100px;
+
+  .q-input {
+    width: 180px;
+  }
+}
+/deep/ .q-item__section--avatar {
+  min-width: 30px;
+}
+/deep/ .q-item {
+  color: $layout-text;
+}
+</style>
