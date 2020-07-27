924ba0b4d34b2eb58efdf1aafeec863df8406930
devsun19151295 <thappatan@heartworks.io>
2020-07-27 14:56:36 +0700

    [ added ] menu item component

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-item :to="to" exact clickable class="full-width q-mt-xs">
+    <q-item-section avatar>
+      <q-icon size="30px" :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label class="text-h6 text-left text-weight-regular">
+        {{ title }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: "MenuItems",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      required: true
+    },
+
+    to: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style></style>
