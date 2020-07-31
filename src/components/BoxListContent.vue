70f64d3e2cb34ddefaced3b425eec0788ac34c12
devsun19151295 <thappatan@heartworks.io>
2020-07-31 16:33:56 +0700

    [ add ] other contents

---
+++ src/components/BoxListContent.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="row justify-center full-width q-pt-md">
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      swipeable
+      animated
+      arrows
+      control-color="primary"
+      height="300px"
+      class="_slide-list"
+    >
+      <template v-for="(item, index) in itemList">
+        <q-carousel-slide :name="index" class="column no-wrap" :key="index">
+          <div class="row justify-center q-gutter-md q-col-gutter no-wrap ">
+            <q-card class="_box-in-slide">
+              <q-card-section horizontal class="full-width full-height">
+                <q-card-section class="full-height items-center ">
+                  <div
+                    class="text-sub-title-color text-overline text-weight-bold text-uppercase"
+                  >
+                    {{ item.firstContent.category }}
+                  </div>
+                  <div class="text-h5 q-mt-sm q-mb-xs _cut-long-text">
+                    {{ item.firstContent.title }}
+                  </div>
+                  <div class="text-caption q-mt-sm text-grey _cut-long-text">
+                    {{ item.firstContent.description }}
+                  </div>
+                </q-card-section>
+
+                <q-card-section class="_box-image col-6 flex flex-center">
+                  <q-avatar size="200px">
+                    <q-img :ratio="1" :src="item.firstContent.imageUrl" />
+                  </q-avatar>
+                </q-card-section>
+              </q-card-section>
+            </q-card>
+            <q-card class="_box-in-slide">
+              <q-card-section horizontal class="full-width full-height">
+                <q-card-section class="full-height items-center ">
+                  <div
+                    class="text-sub-title-color text-overline text-weight-bold text-uppercase"
+                  >
+                    {{ item.secordContent.category }}
+                  </div>
+                  <div class="text-h5 q-mt-sm q-mb-xs _cut-long-text">
+                    {{ item.secordContent.title }}
+                  </div>
+                  <div class="text-caption text-grey _cut-long-text">
+                    {{ item.secordContent.description }}
+                  </div>
+                </q-card-section>
+
+                <q-card-section class="_box-image col-6 flex flex-center">
+                  <q-avatar size="200px">
+                    <q-img :ratio="1" :src="item.secordContent.imageUrl" />
+                  </q-avatar>
+                </q-card-section>
+              </q-card-section>
+            </q-card>
+          </div>
+        </q-carousel-slide>
+      </template>
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["itemList"],
+  data() {
+    return {
+      slide: 0
+    };
+  },
+  created() {
+    console.log(this.itemList);
+  }
+};
+</script>
+
+<style lang="scss">
+._slide-list {
+  width: 100%;
+
+  ._box-in-slide {
+    width: 100%;
+    height: 17rem;
+    box-shadow: none;
+
+    border-radius: 10px;
+    background-color: $grey-9;
+  }
+}
+
+.q-carousel__slides-container {
+  .q-panel {
+    background-color: $grey-10;
+  }
+
+  .q-carousel__slide {
+    padding: unset;
+    padding-top: 16px;
+  }
+}
+
+._cut-long-text {
+  max-height: 128px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+@media (max-width: $breakpoint-sm-max) {
+  ._box-image {
+    display: none;
+  }
+}
+@media (max-width: 1174px) {
+  ._box-image {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/FirstContent.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="_first-content row justify-center full-width">
+    <q-card class="_head-box">
+      <q-card-section>
+        <q-img class="_head-image" :src="imageUrl">
+          <div class="absolute-top text-subtitle1 text-left">
+            <div class="row q-pt-md">
+              <div class="_opty-7 text-weight-bold text-caption text-uppercase">
+                {{ category }}
+              </div>
+            </div>
+            <div class="row q-pt-sm">
+              <div class="text-h5 text-weight-400">{{ title }}</div>
+            </div>
+            <div class="row q-pt-sm">
+              <div class="_opty-7 text-caption">
+                {{ description }}
+              </div>
+            </div>
+          </div>
+        </q-img>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    },
+
+    category: {
+      type: String,
+      default: ""
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+
+    description: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+._first-content {
+  margin-top: -50px;
+}
+
+._head-box {
+  width: 100%;
+  box-shadow: none;
+
+  .q-card__section--vert {
+    padding: unset;
+  }
+
+  ._head-image {
+    height: 22rem;
+
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+  .q-img__content > div {
+    background: unset;
+  }
+}
+</style>
